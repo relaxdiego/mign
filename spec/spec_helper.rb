6faf510a0512b53e0f597259fee844e2635f7ce3
Mark Maglana <mmaglana@gmail.com>
2012-02-27 15:14:43 +0800

    Make specs run even faster

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,7 +37,21 @@ Spork.prefork do
   # need to restart spork for it take effect.
   # This file is copied to spec/ when you run 'rails generate rspec:install'
   ENV["RAILS_ENV"] ||= 'test'
+
+  #=== This part used from http://my.rails-royce.org/2012/01/14/reloading-models-in-rails-3-1-when-usign-spork-and-cache_classes-true/
+  # Use of https://github.com/sporkrb/spork/wiki/Spork.trap_method-Jujutsu
+  Spork.trap_method(Rails::Application, :reload_routes!)
+  Spork.trap_method(Rails::Application::RoutesReloader, :reload!)
+
+  # Prevent main application to eager_load in the prefork block (do not load files in autoload_paths)
+  Spork.trap_method(Rails::Application, :eager_load!)
+
+  # Below this line it is too late...
   require File.expand_path("../../config/environment", __FILE__)
+  # Load all railties files
+  Rails.application.railties.all { |r| r.eager_load! }
+  #===
+
   require 'rspec/rails'
   require 'rspec/autorun'
   require 'capybara/rspec'
