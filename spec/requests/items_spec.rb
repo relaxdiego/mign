4d7676aa5d1c0c3f485b629914f6f7f169c3c3b1
Mark Maglana <mmaglana@gmail.com>
2012-03-05 22:38:43 +0800

    Comment out these steps while we are moving them to cuke

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -1,14 +1,14 @@
-require 'spec_helper'
-
-feature "Items" do
-    background do
-      @item1 = FactoryGirl.create(:item, :subject => 'Item one')
-      @item2 = FactoryGirl.create(:item, :subject => 'Item two')
-    end
-
-    scenario "Items index" do
-      visit "/items"
-      page.should have_content(@item1.subject)
-      page.should have_content(@item2.subject)
-    end
-end
+# require 'spec_helper'
+#
+# feature "Items" do
+#     background do
+#       @item1 = FactoryGirl.create(:item, :subject => 'Item one')
+#       @item2 = FactoryGirl.create(:item, :subject => 'Item two')
+#     end
+#
+#     scenario "Items index" do
+#       visit "/items"
+#       page.should have_content(@item1.subject)
+#       page.should have_content(@item2.subject)
+#     end
+# end
