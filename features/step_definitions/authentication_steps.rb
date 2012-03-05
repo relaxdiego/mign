30f6e20fb2e6401dc754d9d507087aaf6286da82
Mark Maglana <mmaglana@gmail.com>
2012-03-05 22:39:44 +0800

    Install cucumber to project

---
+++ features/step_definitions/authentication_steps.rb
@@ -0,0 +1,10 @@
+When /^(?:he|she) logs in using the correct credentials$/ do
+  visit new_user_session_path
+  fill_in       'user_email',    :with => @user.email
+  fill_in       'user_password', :with => @user.password
+  click_button  'Sign in'
+end
+
+Then /^(?:he|she) should be redirected to the groups page$/ do
+  current_path.should == groups_path
+end
