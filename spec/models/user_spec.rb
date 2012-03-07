be83ab683512c16f7e93d5aec3c14d670f68868f
Mark Maglana <mmaglana@gmail.com>
2012-03-08 06:31:52 +0800

    Add unit testing for User#add_to_owned_groups

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
-# require 'spec_helper'
-#
-# describe User do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+require 'spec_helper'
+
+describe User do
+  it "should add a group to its owned groups" do
+    group = Factory.create(:group)
+    user = Factory.create(:user)
+    user.add_to_owned_groups(group)
+
+    user.owned_groups.include?(group).should be_true
+  end
+end
