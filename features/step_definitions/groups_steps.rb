a7b649e49ab53c5bf55e3dc0d3f6d0176e36eec4
Mark Maglana <mmaglana@gmail.com>
2012-03-07 18:36:26 +0800

    Make sure that the group is actually created

--- features/step_definitions/groups_steps.rb
+++ features/step_definitions/groups_steps.rb
@@ -2,6 +2,9 @@ When /^(?:he|she) creates a group named '(.+)'$/ do |group_name|
   visit new_group_path
   fill_in 'group_name', :with => group_name
   click_button 'Create Group'
+
+  @group = Group.find_by_name(group_name)
+  @group.should_not be_nil
 end
 
 Then /^(?:he|she) will be an owner of that group$/ do
