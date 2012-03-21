82611157818fac6ef6ad3766a501622a5d19e1e8
Mark Maglana <mmaglana@gmail.com>
2012-03-21 21:56:54 +0800

    Fix login steps

--- features/support/authentication_helpers.rb
+++ features/support/authentication_helpers.rb
@@ -2,5 +2,5 @@ def login_with(credentials)
   visit new_user_session_path if current_path != new_user_session_path
   fill_in       'user_email',    :with => credentials[:email]
   fill_in       'user_password', :with => credentials[:password]
-  click_button  'Sign in'
+  click_button  'login'
 end
