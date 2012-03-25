ee232aa8ec782458eb28ba47924e574603ba64d0
Mark Maglana <mmaglana@gmail.com>
2012-03-25 23:58:39 +0800

    Reroute to new_user_session_path instead of root_path after logging out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,11 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to :back, :alert => "You are not allowed to do that"
   end
+
+  private
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
